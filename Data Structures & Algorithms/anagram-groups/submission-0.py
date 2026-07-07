3027748addc86ea1e7809359f28e05e7bac1abea
Pradhuman Kumar Soni <pradhumansoni2004@gmail.com>
2026-07-07 13:13:13 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -0,0 +1,12 @@
+from collections import Counter
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # Method 1: Hashing Based on Sorted String
+        hash= {}
+        for word in strs:
+            key = ''.join(sorted(word)) 
+            if key not in hash:
+                hash[key] = [word] 
+            else:
+                hash[key] = hash[key] + [word]
+        return list(hash.values())
